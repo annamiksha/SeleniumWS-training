6ff569f774a09f81c5f0ddceda5ed7ad02faa720
Anna Miksha <miksha.av@gmail.com>
2017-06-12 18:09:40 +0500

    task12 add file

--- java-example/src/test/java/task12.java
+++ java-example/src/test/java/task12.java
@@ -9,7 +9,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.interactions.Actions;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertTrue;
 
 public class task12 {
     private WebDriver driver;
@@ -24,23 +30,32 @@ public class task12 {
 
     @Test
     public void test1(){
+        File file = new File ("7408_1.jpg");
+        String filePath = file.getAbsolutePath();
+
+        Random rand = new Random();
+        String rn = "test" + rand.nextInt(100000);
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         AdminPanelLogin();
         openAdminBlock(2);
+        int productsBefore = countRootItems();
+
         driver.findElement(By.cssSelector(".pull-right li:last-child")).click();
         WebElement tab = driver.findElement(By.cssSelector(".tab-content"));
+
 
         //General
         tab.findElement(By.cssSelector("[data-toggle = buttons] .btn-default")).click();
         tab.findElement(By.cssSelector("[data-name = Subcategory]")).click();
+        tab.findElement(By.cssSelector("[data-name = Subcategory]")).click();
         tab.findElement(By.name("product_groups[]")).click();
         setData(tab,"date_valid_from", "21052017");
         setData(tab,"date_valid_to", "21052020");
-        setData(tab,"code","product_code");
+        setData(tab,"code","product_code" + rn);
         tab.findElement(By.name("name[en]")).sendKeys("New Product");
-        setData(tab,"sku","sku1");
-        setData(tab,"gtin","gtin1");
-        setData(tab,"taric","taric1");
+        setData(tab,"sku","sku1" + rn);
+        setData(tab,"gtin","gtin1"+ rn);
+        setData(tab,"taric","taric1" + rn);
 
         WebElement quantity = tab.findElement(By.name("quantity"));
         quantity.click();
@@ -75,37 +90,49 @@ public class task12 {
         status.click();
         status.findElements(By.tagName("option")).get(2).click();
 
+        tab.findElement(By.name("new_images[]")).sendKeys(filePath);
+
+
         //Information
-//        driver.findElement(By.cssSelector(".nav-tabs li:nth-child(2)")).click();
-//        tab = driver.findElement(By.cssSelector(".tab-content"));
-//        tab.findElement(By.name("manufacturer_id")).click();
-//        tab.findElements(By.cssSelector("[name = manufacturer_id] option")).get(1).click();
-//        setData(tab,"keywords","test");
-//        tab.findElement(By.name("short_description[en]")).sendKeys("Short description is short");
-//        tab.findElement(By.className("trumbowyg-editor")).click();
-//        tab.findElement(By.className("trumbowyg-editor")).sendKeys("There was once a couple who had long in vain wished for a child. At length the woman hoped that God was about to grant her desire. They had a little window at the back of their house from which a splendid garden could be seen, which was full of beautiful flowers and herbs. It was, however, surrounded by a high wall, and no one dared to go into it because it belonged to an enchantress, who had great power and was dreaded by all the world.");
-//        tab.findElement(By.name("attributes[en]")).sendKeys("attribute1, attribute2, attribute3");
-//        tab.findElement(By.name("head_title[en]")).sendKeys("Head Title");
-//        tab.findElement(By.name("meta_description[en]")).sendKeys("Meta Description");
+        driver.findElement(By.cssSelector(".nav-tabs li:nth-child(2)")).click();
+        tab = driver.findElement(By.cssSelector(".tab-content"));
+        tab.findElement(By.name("manufacturer_id")).click();
+        tab.findElements(By.cssSelector("[name = manufacturer_id] option")).get(1).click();
+        setData(tab,"keywords","test");
+        tab.findElement(By.name("short_description[en]")).sendKeys("Short description is short");
+        tab.findElement(By.className("trumbowyg-editor")).click();
+        tab.findElement(By.className("trumbowyg-editor")).sendKeys("There was once a couple who had long in vain wished for a child. At length the woman hoped that God was about to grant her desire. They had a little window at the back of their house from which a splendid garden could be seen, which was full of beautiful flowers and herbs. It was, however, surrounded by a high wall, and no one dared to go into it because it belonged to an enchantress, who had great power and was dreaded by all the world.");
+        tab.findElement(By.name("attributes[en]")).sendKeys("attribute1, attribute2, attribute3");
+        tab.findElement(By.name("head_title[en]")).sendKeys("Head Title");
+        tab.findElement(By.name("meta_description[en]")).sendKeys("Meta Description");
+
 
         //Prices
-//        driver.findElement(By.cssSelector(".nav-tabs li:nth-child(3)")).click();
-//        tab = driver.findElement(By.cssSelector(".tab-content"));
-//
-//        WebElement purchase_price = tab.findElement(By.name("purchase_price"));
-//        purchase_price.clear();
-//        purchase_price.sendKeys("25");
-//
-//        WebElement currency_code = tab.findElement(By.name("purchase_price_currency_code"));
-//        currency_code.click();
-//        currency_code.findElement(By.cssSelector("[value = EUR]")).click();
-//
-//        WebElement price = tab.findElement(By.name("prices[USD]"));
-//        price.clear();
-//        price.sendKeys("12");
-//        price = tab.findElement(By.name("prices[EUR]"));
-//        price.clear();
-//        price.sendKeys("10");
+        driver.findElement(By.cssSelector(".nav-tabs li:nth-child(3)")).click();
+        tab = driver.findElement(By.cssSelector(".tab-content"));
+
+        WebElement purchase_price = tab.findElement(By.name("purchase_price"));
+        purchase_price.clear();
+        purchase_price.sendKeys("25");
+
+        WebElement currency_code = tab.findElement(By.name("purchase_price_currency_code"));
+        currency_code.click();
+        currency_code.findElement(By.cssSelector("[value = EUR]")).click();
+
+        WebElement price = tab.findElement(By.name("prices[USD]"));
+        price.clear();
+        price.sendKeys("12");
+        price = tab.findElement(By.name("prices[EUR]"));
+        price.clear();
+        price.sendKeys("10");
+
+
+        //Saving
+        driver.findElement(By.name("save")).click();
+        driver.findElement(By.cssSelector(".center-block")).click();
+        openAdminBlock(2);
+        int productsAfter = countRootItems();
+        assertTrue(productsBefore == productsAfter - 1);
 
 
     }
@@ -125,9 +152,16 @@ public class task12 {
         element.findElement(By.cssSelector("[name = " + fieldName + "]")).sendKeys(value);
     }
 
+    int countRootItems(){
+        WebElement productTable = driver.findElement(By.className("data-table"));
+        List<WebElement> products = new ArrayList<>();
+        products = productTable.findElements(By.tagName("tr"));
+        return products.size();
+    }
+
     @After
     public void stop(){
-//        driver.quit();
-//        driver = null;
+        driver.quit();
+        driver = null;
     }
 }
