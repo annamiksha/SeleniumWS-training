20c2e749fedc7453cf76f10cf8c33211fa8fbe0b
Anna Miksha <miksha.av@gmail.com>
2017-06-13 23:27:00 +0500

    task13 fix

--- java-example/src/test/java/task13.java
+++ java-example/src/test/java/task13.java
@@ -6,16 +6,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
-import static junit.framework.TestCase.assertTrue;
 import static org.openqa.selenium.support.ui.ExpectedConditions.attributeToBe;
 import static org.openqa.selenium.support.ui.ExpectedConditions.stalenessOf;
-import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
+
 
 public class task13 {
     private WebDriver driver;
